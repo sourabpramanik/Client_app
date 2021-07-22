5b4c33589c88fb9f578f5303b27885401e285ee0
Sourab Pramanik <sourabpramanik2011@outlook.com>
2021-07-22 10:35:29 +0530

    Update

--- src/components/Menu/Menu.elements.js
+++ src/components/Menu/Menu.elements.js
@@ -14,15 +14,18 @@ flex-direction:row;
 align-items:center;
 justify-content:space-between;
 padding: 20px 30px;
-border-bottom: 1px solid #C0C0C0;
+
 `
 export const TabItem = styled.div`
 color: #BBBBBB;
 font-size:15px;
 font-weight:700;
 cursor:pointer;
+box-shadow: -5px 5px 10px rgba(229, 229, 229, 0.2), 5px -5px 10px rgba(229, 229, 229, 0.2), -5px -5px 10px rgba(253, 253, 253, 0.9), 5px 5px 13px rgba(229, 229, 229, 0.9), inset 1px 1px 2px rgba(253, 253, 253, 0.3), inset -1px -1px 2px rgba(229, 229, 229, 0.5);
+border-radius: 0px 0px 15px 15px;
 &:focus, &:hover{
-    color: #3CBCB4
+    color: #3CBCB4;
+    border-radius: 0px 0px -15px -15px;
 }
 `
 export const MenuContent = styled.div`
@@ -30,4 +33,5 @@ height:100%;
 width:100%;
 margin-top:30px;
 /* margin: 0 20px; */
+
 `
